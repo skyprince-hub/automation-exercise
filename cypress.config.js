45fb0750afd1405f2c68efde3ae21a24c3c751db
John Prince <johnprince.dev@gmail.com>
2026-02-23 21:22:54 +0800

    Set up some configuration to record, and take screenshots

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,15 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    // ULR of the automationexercises 
+    baseUrl: "https://automationexercise.com/",
+
+     // record videos (true = always, false = only failures, 'suppress-flaky' = smart recording)
+     video: true,
+
+     // take screenshots on failure
+     screenshotOnRunFailure: true,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
