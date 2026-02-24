8f88f12cf2688adcc475c33918c0d756053b7bad
John Prince <johnprince.dev@gmail.com>
2026-02-24 22:38:18 +0800

    Initial commit

--- cypress/e2e/auth/login-user.cy.js
+++ cypress/e2e/auth/login-user.cy.js
@@ -1,5 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Login User', () => {
+  
+  it('Login User with Correct Email and Password', () => {
     cy.visit('https://example.cypress.io')
+  })
+
+  it('Login User with Incorrect Email and Password',() => {
+
+  })
+  
+  it('Logout User', () => {
+
   })
 })
