e3b6818d8af4ba9370967820d9d7e34010f04fd1
John Prince <johnprince.dev@gmail.com>
2026-02-24 22:38:38 +0800

    Test Case 1 DONE

--- cypress/e2e/auth/register-user.cy.js
+++ cypress/e2e/auth/register-user.cy.js
@@ -1,5 +1,59 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Register User', () => {
+  let newUser;
+
+  // define javascript object representing a valid existing user
+  const existingUser = {
+    name: 'John Doe',
+    email: 'johndoe.cy@example.com',
+    password: 'password'
+  };
+
+  // randome number to determine if title is Mr or Mrs
+  const randomTitle = (Math.floor(Math.random() * 10) + 1) % 2 ? 'Mr.' : 'Mrs.';
+
+  // random number from 1 - 7 to select which to pick among the 7 country
+  const country = ['India', 'United States', 'Canada', 'Australia', 'Israrel', 'New Zealand', 'Singapore'];
+  const randomCountry = country[Math.floor(Math.random() * country.length)];
+
+  beforeEach(() => {
+    newUser = {
+      title: randomTitle,
+      name: 'newUser' + new Date().getSeconds(), 
+      lastname: 'newUser' + new Date().getSeconds(),
+      email: 'newUser' + new Date().getSeconds() + '@example.com',
+      password: 'password' + new Date().getSeconds(),
+      day: '03',
+      month: 'February',
+      year: '2004',
+      company: 'company',
+      address: 'address' + new Date().getSeconds(),
+      country: randomCountry,
+      state: 'state',
+      city: 'city',
+      zipcode: '123',
+      mobile_number: new Date().getSeconds()
+    };
+
+    // before each test, always visit the landing page
+    cy.visit('/', { failOnStatusCode: false});
+  });
+  
+  it('Register User with Valid Credentials', () => {
+    cy.verifyURL();
+    cy.contentPage('AutomationExercise');
+    cy.signUp();
+    cy.contentPage('New User Signup!');
+    cy.register(newUser.name, newUser.email);
+    cy.contentPage('Enter Account Information');
+    cy.fullSignUp(newUser);
+    cy.contentPage('ACCOUNT CREATED!');
+    cy.clickButton('continue-button');
+    cy.clickLink('/delete_account');
+    cy.contentPage('ACCOUNT DELETED!');
+    cy.clickButton('continue-button');
+  });
+
+  it('Register User with Existing Email', () => {
+
+  });
 })
