0dcacb8e7be16e63632de31cd86386ad97d84149
John Prince <johnprince.dev@gmail.com>
2026-02-27 13:12:08 +0800

    Added the test case for api page

--- cypress/e2e/test-cases/verify-api-testcases.cy.js
+++ cypress/e2e/test-cases/verify-api-testcases.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Verify API Testing Page', () => {
+
+  beforeEach(() => {
+    cy.visit('/', {failOnStatusCode: false});
+  })
+
+  it('should display all api test to practice', () => {
+    cy.verifyURL();
+    cy.contentPage('AutomationExercise');
+    cy.clickLink('/api_list');
+    cy.verifyURL('api_list');
   })
 })
