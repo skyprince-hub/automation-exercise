127e948bef50e97bfda9f6001446d42422d43b8c
John Prince <johnprince.dev@gmail.com>
2026-03-08 23:26:41 +0800

    Finish the TC8

--- cypress/e2e/products/verify-product.cy.js
+++ cypress/e2e/products/verify-product.cy.js
@@ -1,5 +1,34 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Verify Products', () => {
+
+  beforeEach(() => {
+    cy.visit('/', { failOnStatusCode: false});
+  });
+
+  it.only('Verify All Products and product detail page ', () => {
+    cy.verifyURL();
+    cy.contentPage('AutomationExercise');
+    cy.clickLink('/products');
+    cy.contentPage('ALL PRODUCTS');
+    cy.get('.features_items').within(() => {
+      cy.contentPage('Rs');
+    });
+    cy.clickLink('/product_details');
+    cy.verifyURL('product_details/');
+    cy.get('.product-information')
+      .inContainer('Category')
+      .inContainer('Rs')
+      .inContainer('Availability')
+      .inContainer('Condition')
+      .inContainer('Brand');
+  });
+
+  it('Search Product', () => {
+
+  });
+
+  it('Search Products and Verify Cart After Login', () => {
+    cy.fixtures('user').then(({existingUser}) => {
+        cy.login(existingUser.email, existingUser.password);
+    });
   })
 })
