11ebb8b1bd628ba769c70bcaab3d769efd3d9fe9
John Prince <johnprince.dev@gmail.com>
2026-03-12 23:39:08 +0800

    Finished verify-product test suit

--- cypress/e2e/products/verify-product.cy.js
+++ cypress/e2e/products/verify-product.cy.js
@@ -1,6 +1,8 @@
 describe('Verify Products', () => {
 
   beforeEach(() => {
+    cy.fixture('product').as('products');
+    cy.fixture('user').as('users');
     cy.visit('/', { failOnStatusCode: false});
   });
 
@@ -22,7 +24,7 @@ describe('Verify Products', () => {
       .inContainer('Brand');
   });
 
-  it.only('Search Product', () => {
+  it('Search Product', () => {
     cy.verifyURL();
     cy.contentPage('AutomationExercise');
     cy.clickLink('/products');
@@ -36,10 +38,23 @@ describe('Verify Products', () => {
     cy.get('.features_items').inContainer('Tshirt');
   });
 
-  it('Search Products and Verify Cart After Login', () => {
-    //fixtures can be found in the fixture/user.json
-    cy.fixture('user').then(({existingUser}) => {
-        cy.login(existingUser.email, existingUser.password);
-    });
+  it.only('Search Products and Verify Cart After Login', function() {
+    const {womensProduct} = this.products;
+    const {existingUser} = this.users;
+
+    cy.clickLink('/products');
+    cy.contentPage('ALL PRODUCTS');
+    cy.get('#search_product').type(womensProduct.name);
+    cy.clickButton('submit_search', 'id');
+    cy.contentPage('SEARCHED PRODUCTS');
+    cy.clickButton('add-to-cart', 'class')
+    //cy.clickLink('/view_cart');
+    cy.clickButton('close-modal', 'class');
+    cy.clickLink('/view_cart');
+    cy.contentPage(womensProduct.name);
+    cy.clickLink('/login')
+    cy.login(existingUser.email, existingUser.password);
+    cy.clickLink('/view_cart');
+    cy.contentPage(womensProduct.name);
   })
 })
