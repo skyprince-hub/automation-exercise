1462e09331a883517e7acde8391f9eae23a1a784
John Prince <johnprince.dev@gmail.com>
2026-03-15 17:17:20 +0800

    Finished the product category test suits

--- cypress/e2e/products/product-category.cy.js
+++ cypress/e2e/products/product-category.cy.js
@@ -4,8 +4,8 @@ describe('Product Categories', () => {
     cy.verifyURL();
   });
 
-  it.only('View Category Products', () => {
-    cy.get('.left-sidebar').inCointainer('CATEGORY');
+  it('View Category Products', () => {
+    cy.get('.left-sidebar').inContainer('CATEGORY');
     cy.contains('.panel-title a', 'Women').click();
     cy.clickLink('/category_products/1');
     cy.contentPage('WOMEN - DRESS PRODUCTS');
@@ -15,6 +15,22 @@ describe('Product Categories', () => {
   });
 
   it('View and Cart Brand Products', () => {
+    cy.clickLink('/products');
+    cy.get('.brands_products').inContainer('BRANDS');
+    cy.clickLink('/brand_products/Polo');
+    cy.get('.features_items')
+      .inContainer('BRAND - POLO PRODUCTS')
+      .contains('Rs').should('be.visible')
+    ;
+    cy.contains('a', 'Add to cart')
+    cy.contains('a', 'View Product')
+    cy.clickLink('/brand_products/H&M');
+    cy.get('.features_items')
+      .inContainer('BRAND - H&M PRODUCTS')
+      .contains('Rs').should('be.visible')
+    ;
+    cy.contains('a', 'Add to cart')
+    cy.contains('a', 'View Product')
 
   });
 })
