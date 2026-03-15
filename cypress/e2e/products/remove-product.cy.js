374192162d10de101c93848273aac6592a00c61a
John Prince <johnprince.dev@gmail.com>
2026-03-15 21:29:06 +0800

    Finished adding test case for the remove-product test suit

--- cypress/e2e/products/remove-product.cy.js
+++ cypress/e2e/products/remove-product.cy.js
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Removing of Products', () => {
+  beforeEach(() => {
+    cy.visit('/', {failOnStatus: false});
+    cy.verifyURL();
+    cy.fixture('product').as('products');
+  });
+  it('Remove Products From Cart', function() {
+    const {firstProduct} = this.products;
+
+    cy.contentPage('AutomationExercise');
+    cy.clickButton('1', 'data-product-id');
+    cy.clickButton('modal', 'data-dismiss');
+    cy.clickLink('/view_cart');
+    cy.get('tr#product-1')
+      .inContainer(firstProduct.name)
+      .inContainer(firstProduct.price)
+    ;
+    cy.clickButton('1','data-product-id');
+    cy.contentPage('Cart is empty!');
   })
 })
