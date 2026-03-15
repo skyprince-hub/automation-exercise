9563064b3238e3acaf60a9cc4dd790dc3aa22840
John Prince <johnprince.dev@gmail.com>
2026-03-15 17:53:31 +0800

    Finish adding test case for the Add Review test suit

--- cypress/e2e/products/add-review.cy.js
+++ cypress/e2e/products/add-review.cy.js
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Add Reviews', () => {
+  beforeEach(() => {
+    cy.visit('/', {failOnStatusCode: false});
+    cy.verifyURL();
+    cy.fixture('user').as('users');
+  });
+  
+  it('Add Review on Product', function() {
+    const {existingUser} = this.users;
+
+    cy.clickLink('/products');
+    cy.get('.features_items').inContainer('ALL PRODUCTS');
+    cy.clickLink('/product_details/1');
+    cy.get('#review').should('have.attr', 'placeholder', 'Add Review Here!');
+    cy.get('#name').type(existingUser.name);
+    cy.get('#email').type(existingUser.email);
+    cy.get('#review').type('This is a review from Cypress!');
+    cy.clickButton('button-review', 'id');
+    cy.contentPage('Thank you for your review.')
   })
 })
