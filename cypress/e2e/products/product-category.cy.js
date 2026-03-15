fbb9cfbda4135a79775adbf02cd1c88b4346c341
John Prince <johnprince.dev@gmail.com>
2026-03-15 16:47:32 +0800

    initial commit

--- cypress/e2e/products/product-category.cy.js
+++ cypress/e2e/products/product-category.cy.js
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Product Categories', () => {
+  beforeEach(() => {
+    cy.visit('/', {failOnStatusCode: false});
+    cy.verifyURL();
+  });
+
+  it.only('View Category Products', () => {
+    cy.get('.left-sidebar').inCointainer('CATEGORY');
+    cy.contains('.panel-title a', 'Women').click();
+    cy.clickLink('/category_products/1');
+    cy.contentPage('WOMEN - DRESS PRODUCTS');
+    cy.contains('.panel-title a', 'Men').click();
+    cy.clickLink('/category_products/3');
+    cy.contentPage('MEN - TSHIRTS PRODUCTS');
+  });
+
+  it('View and Cart Brand Products', () => {
+
+  });
 })
