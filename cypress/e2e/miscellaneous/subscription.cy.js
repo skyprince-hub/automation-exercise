928a0864ca1f235b83f9f01ac47534cac016017a
John Prince <johnprince.dev@gmail.com>
2026-03-16 09:40:03 +0800

    Finished adding test case for the subscription test suit

--- cypress/e2e/miscellaneous/subscription.cy.js
+++ cypress/e2e/miscellaneous/subscription.cy.js
@@ -1,5 +1,33 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Subscriptions', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.verifyURL();
+    cy.fixture('user').as('users');
+  });
+
+  it('Verify Subscription in Home Page', function() {
+    const {existingUser} = this.users;
+
+    cy.contentPage('AutomationExercise');
+    cy.get('#footer').inContainer('Subscription')
+    cy.get('#susbscribe_email').type(existingUser.email);
+    //cy.clickButton('subscribe', 'id');
+    cy.get('.searchform').submit();
+    cy.get('#success-subscribe', {timeout:10000})
+      .should('be.visible')
+      .and('contain', 'You have been successfully subscribed!');
+  });
+
+  it('Verify Subscription in Cart Page', function() {
+    const {existingUser} = this.users;
+    cy.contentPage('AutomationExercise');
+    cy.clickLink('view_cart');
+    cy.get('#footer').inContainer('Subscription')
+    cy.get('#susbscribe_email').type(existingUser.email);
+    //cy.clickButton('subscribe', 'id');
+    cy.get('.searchform').submit();
+    cy.get('#success-subscribe', {timeout:10000})
+      .should('be.visible')
+      .and('contain', 'You have been successfully subscribed!');
   })
 })
